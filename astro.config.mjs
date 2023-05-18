b3ca6284d9cb001808a604d2578dc7c238148dd7
galzamora <gustavoalzamora1997@gmail.com>
2023-05-18 18:55:11 -0300

    config file updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ const LIVE_URL = "https://yourwebsiteurl.com";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
-const server = process.env.SERVER;
+
 let BASE_URL = LOCALHOST_URL;
 // When you're building your site in local or in CI, you could just set your URL manually
 if (isBuild) {
